4220ab5d0f7b7b3082c7e0ec9b5b244b7cae0929
UTAKAYUKI <t_ueda@tax-subaru.com>
2026-05-09 08:08:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE = 'vocab-v1';
+const CACHE = 'vocab-v3';
 const FILES = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
